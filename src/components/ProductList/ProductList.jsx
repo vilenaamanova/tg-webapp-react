42c0f96f0fd9cfee5cac0c1ef0859c4ee2047245
Vilena <vilenaamanova@gmail.com>
2024-06-27 23:13:03 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -37,7 +37,7 @@ const ProductList = () => {
             },
             body: JSON.stringify(data)
         })
-    }, [])
+    }, [addedItems])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
