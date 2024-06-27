f85104d81141834985d8853739a1865f92ae67da
Vilena <vilenaamanova@gmail.com>
2024-06-27 23:07:17 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://localhost:8000', {
+        fetch('https://192.168.0.132:8000', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
